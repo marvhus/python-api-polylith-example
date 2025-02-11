3aed5e59b179a9f31fe7ab828763c717e20da360
Martin <martin@marvhus.xyz>
2025-02-11 19:02:22 +0100

    Improvements to running projects
    - Moved run.sh to root
    - Simplified run.sh
    - Added requirements.txt to gitignore

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Exit on error
+set -e
+
+# Check for argument
+if [ -z "$1" ]; then
+    echo "Usage:  $0 <project>"
+    exit 1
+fi
+
+PROJECT=$1
+
+#Check if project exits
+PROJECT_DIR="projects/${PROJECT}"
+if [ ! -d "$PROJECT_DIR" ]; then
+    echo "Unable to find project:  $PROJECT_DIR"
+    exit 1
+fi
+
+echo "=============== Running project ==============="
+uv sync
+uv run uvicorn example.${PROJECT}.core:app --reload
