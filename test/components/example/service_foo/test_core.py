1ec30860d05c2980ef125c13e4985f6ec3dce916
Martin <martin@marvhus.xyz>
2025-02-11 23:56:16 +0100

    Add test for service_foo

--- test/components/example/service_foo/test_core.py
+++ test/components/example/service_foo/test_core.py
@@ -3,3 +3,10 @@ from example.service_foo import core
 
 def test_sample():
     assert core is not None
+
+
+def test_retrieve_some_data():
+    assert core.retrieve_some_data("a") == [1, 2, 3]
+    assert core.retrieve_some_data("b") == [4, 5, 6]
+    assert core.retrieve_some_data("c") == [7, 8, 9]
+    assert core.retrieve_some_data("d") is None
