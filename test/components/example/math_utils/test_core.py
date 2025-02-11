35bf6fd4165ec7157a23fbe07f9d70dc8c6772dc
Martin <martin@marvhus.xyz>
2025-02-11 20:27:40 +0100

    Add basic test
    Run tests with:  uv run pytest

--- test/components/example/math_utils/test_core.py
+++ test/components/example/math_utils/test_core.py
@@ -3,3 +3,12 @@ from example.math_utils import core
 
 def test_sample():
     assert core is not None
+
+def test_fibbonacci():
+    expected = [0, 1, 1, 2, 3, 5, 8, 13]
+    for index, expect in enumerate(expected):
+        assert core.fib(index) is expect
+
+def test_fibbonacci_invalid():
+    assert core.fib(None) is None
+    assert core.fib(-1) is None
